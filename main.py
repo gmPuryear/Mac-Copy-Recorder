ae7f7242598ce20e4fcfb2b3bac5a3a9462f121b
matt.puryear <georgem.puryear@gmail.com>
2025-04-01 22:49:20 -0500

    edited gitignore to ignore virtual env files

--- main.py
+++ main.py
@@ -1,34 +1,4 @@
-# import pyperclip
-
-# def pyperclipCopyPaste():
-#     pyperclip.copy("Hello world!")
-#     pyperclip.paste()
-
-# def main():
-#     pyperclipCopyPaste()
-
-
-# if __name__ == "__main__":
-#     main()
-
-# mclip.py - A multi-clipboard program
-
-
 import sys
 import pyperclip
+import pynput
 
-TEXT = {'agree': """Yes, I agree. That sounds fine to me.""",
-        'busy': """Sorry, we can do this later this week or next week?""",
-        'upsell': """Would you consider making this a monthly donation?"""}
-
-if len(sys.argv) < 2:
-    print("Usage: python main.py [keyphrase] - copy phrase text")
-    sys.exit()
-
-keyphrase = sys.argv[1] # first command line is the keyphrase
-
-if keyphrase in TEXT:
-    pyperclip.copy(TEXT[keyphrase])
-    print("Text for " + keyphrase + " copied to clipboard.")
-else:
-    print("There is no text for " + keyphrase)
