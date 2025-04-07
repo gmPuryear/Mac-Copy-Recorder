036c9e46121685e511346c080b15e77fcd151b77
George Puryear <gm.puryear@yahoo.com>
2025-04-07 08:36:14 -0500

    added function to open and write copied contents to the file

--- README.md
+++ README.md
@@ -1,20 +1,23 @@
 # Mac-Copy-Recorder
+
 Everytime you copy text on a mac, the script will save it to a text file for later use.
 
 ## Notes
 
 ### Functionality:
+
 -When user copys something either from key strokes (command+c) or right/control click, the copied
 content will be saved to an external file.
 -External file will be saved for a period of a month then deleted.
 -Time stamps added to each entry in the external file.
 -Create GUI that will display all the previously copied material
+-Python gui when launched will automatically show the clipboard history
 
 ### Packages/Libraries:
+
 os: for file IO (already a part of python standard library)
 sys (maybe):
 pynput (library): mouse and keyboard input and monitoring. pip install pynput
-
-
-
-
+pyperclip: for copy paste functionality
+launchd: for managing system services and daemons
+Tkinter: for building GUI with python
--- main.py
+++ main.py
@@ -2,8 +2,54 @@ import sys
 import pyperclip
 from pynput import keyboard
 
+COPY_PASTE_FILE_PATH = "~/Documents/mac-copy-recorder-data.txt"
+
+
+#prints message to console when hotkeys are presssed
+def on_activate():
+    print('Global hotkey activated!')
+
+# if hotkeys (command + c) are pressed then call write to file func and passes copied value
+def listen_for_hotkeys():
+    try:
+    # creates a HotKey oject from the HotKey class so pynput can read the string
+        hotkey = keyboard.HotKey(
+            keyboard.HotKey.parse('<cmd>+c'),
+            on_activate)
+        # creates a listener object that listens for hotkeys to be pressed and released. The 'with' ensures
+        # when the listener is stopped or the program ends, resources are cleaned up.
+        with keyboard.Listener(
+                on_press = hotkey.press,
+                on_release = hotkey.release) as listener:
+            # Blocks the program from continuing until the listener is explicitly stopped and listens
+            # indefinitely.
+            listener.join()
+    except Exception as e:
+        print(f"Error in hotkey listener: {e}")
+
+def open_file_to_write():
+    try:
+        # if file does not existr, create and open
+        with open(COPY_PASTE_FILE_PATH, 'a') as file_to_write:
+            print("hello!")
+            # 'write_to_file' function to write to file taking in copied value from 'listen' function
+            # 'date_time' variable for each value writen to file
+            # open file to write to and write value from 'listen' function
+            # throws exception for errors
+    except Exception as e:
+        print(f"Error writing to mac-copy-recorder-data.txt: {e}")
+
+
+
+
+# 'write_to_file' function to write to file taking in copied value from 'listen' function
+    # 'date_time' variable for each value writen to file
+    # open file to write to and write value from 'listen' function
+    # throws exception for errors
+
+
 def main():
-    print("hello")
+    listen_for_hotkeys()
 
 if __name__ == "__main__":
     main()
