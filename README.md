21a7703b466796fa7687e3a2486ba1405fa8875c
George Puryear <gm.puryear@yahoo.com>
2025-04-20 08:10:58 -0500

    added p-list to be able to use system userAgents to use launchd

--- README.md
+++ README.md
@@ -21,3 +21,12 @@ pynput (library): mouse and keyboard input and monitoring. pip install pynput
 pyperclip: for copy paste functionality
 launchd: for managing system services and daemons
 Tkinter: for building GUI with python
+
+### Setup
+
+launchd differentiates between agents and daemons. The main difference is that an agent is run on behalf of the logged in user while a daemon runs on behalf of the root user or any user you specify with the UserName key. Only agents have access to the macOS GUI.
+
+-Store the .plist file in ~/Library/LaunchAgents if you want script to run only for the current
+logged in user
+-Store the script in ~/Scripts
+-Make the script exectutable with 'chmod +x ~/Scripts/script.py' this will allow the script to be run by a cron job/launchd type srevice
---
+++ mac-copy-recorder.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import sys
+import pyperclip
+import os
+from pynput import keyboard
+from datetime import datetime
+from tzlocal import get_localzone
+import time
+
+
+COPY_PASTE_FILE_PATH = os.path.expanduser("~/Documents/mac-copy-recorder-data.txt")
+
+
+#prints message to console when hotkeys are presssed
+def on_activate():
+    print('Global hotkey activated!')
+    time.sleep(0.1)  # Add a short delay to allow the clipboard to update
+    copied_content = pyperclip.paste()
+    copied_content_dict = {
+        'date_time': datetime.now(get_localzone()).strftime('%Y-%m-%d %H:%M:%S'),
+        'copied_content': copied_content
+    }
+    write_to_file(copied_content_dict['date_time'] + ': ' + copied_content_dict['copied_content'] + '\n')
+
+# hotkeys (command + c) are pressed then call write to file func and passes copied value
+def listen_for_hotkeys():
+    try:
+    # creates a HotKey oject from the HotKey class so pynput can read the string
+        hotkey = keyboard.HotKey(
+            keyboard.HotKey.parse('<cmd>+c'),
+            on_activate)
+        # creates a listener object that listens for hotkeys to be pressed and released. The 'with' ensures
+        # when the listener is stopped or the program ends, resources are cleaned up.
+        with keyboard.Listener(
+                on_press = hotkey.press,
+                on_release = hotkey.release) as listener:
+            # Blocks the program from continuing until the listener is explicitly stopped and listens
+            # indefinitely.
+            listener.join()
+    except Exception as e:
+        print(f"Error in hotkey listener: {e}")
+
+def write_to_file(copied_value):
+    try:
+        # if file does not exist, create and open in apend mode
+        # 'with' closes the file automatically
+        with open(COPY_PASTE_FILE_PATH, 'a') as file_to_write:
+            file_to_write.write(copied_value + '\n')
+            # 'date_time' variable for each value writen to file
+            # throws exception for errors
+    except Exception as e:
+        print(f"Error writing to file: {e}")
+
+def main():
+    listen_for_hotkeys()
+
+if __name__ == "__main__":
+    main()
+
